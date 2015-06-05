93533884a2b57551ee9d3f43e93e2a7d19a617e2
Jonas Kirkemyr <jonas@kirkemyr.no>
2015-06-05 14:32:47 +0200

    updated code according to feedback

--- Portofolio/app/src/main/java/nano/jonask/portofolio/Portofolio.java
+++ Portofolio/app/src/main/java/nano/jonask/portofolio/Portofolio.java
@@ -2,8 +2,6 @@ package nano.jonask.portofolio;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -11,6 +9,7 @@ import android.widget.Toast;
 
 public class Portofolio extends Activity {
 
+    private Toast msgToast;
 
     private static String startMsg="This button will launch ";
 
@@ -20,36 +19,19 @@ public class Portofolio extends Activity {
         setContentView(R.layout.activity_portofolio);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_portofolio, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
 
     public void onButtonClick(View button)
     {
-        Toast.makeText(
-                getApplicationContext(),
-                (this.startMsg+((Button)button).getText().toString()),
-                Toast.LENGTH_SHORT
-        ).show();
+        if(msgToast!=null)
+            msgToast.cancel();
+
+        msgToast= Toast.makeText(
+                    this,
+                    (Portofolio.startMsg+((Button)button).getText().toString()),
+                    Toast.LENGTH_SHORT
+                );
+
+        msgToast.show();
 
     }
 }
